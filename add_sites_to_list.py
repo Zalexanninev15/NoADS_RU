ea12c9ae6279c84691134813612af4e1f16a350f
Zalexanninev15 <blue.shark@disroot.org>
2022-01-13 12:00:00 +0300

    🔥 Автодобавление сайтов в список
    Написал скрипт для автоматического добавления сайтов в список: https://github.com/Zalexanninev15/NoADS_RU/blob/main/SITES.md

--- add_sites_to_list.py
+++ add_sites_to_list.py
@@ -10,8 +10,8 @@ for matchNum, match in enumerate(matches, start=1):
     link = f"-{match}"
     links.append(link)
     print(link)
-with open('SITES.md', 'w') as file:
-    file.write('\n# Сайты, которые имеются в списке NoADS_RU\n')
+with open('SITES.md', 'w', encoding="utf8") as file:
+    file.write('\n# Сайты, которые имеются в списке NoADS_RU:\n')
     for i in links:
         file.write(f"\n{i}")
 print('Список пополнился новыми сайтами!')
