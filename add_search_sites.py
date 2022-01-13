68d3289e697f1006542e5fb78fa2d885f817a257
Zalexanninev15 <blue.shark@disroot.org>
2022-01-13 12:04:35 +0300

    🔥 Автодобавление сайтов в список
    Написал скрипт для автоматического добавления сайтов в список

---
+++ add_search_sites.py
@@ -0,0 +1,17 @@
+import re
+
+regex = r" https:\/\/[a-z]\w+(.+)$"
+with open('ads_list.txt', 'r') as file:
+    content = file.read()
+matches = re.finditer(regex, content, re.MULTILINE)
+links = []
+for matchNum, match in enumerate(matches, start=1):
+    match = match.group()
+    link = f"-{match}"
+    links.append(link)
+    print(link)
+with open('SITES.md', 'w') as file:
+    file.write('\n# Сайты, которые имеются в списке NoADS_RU\n')
+    for i in links:
+        file.write(f"\n{i}")
+print('Список пополнился новыми сайтами!')
