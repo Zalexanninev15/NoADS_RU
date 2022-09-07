b36e05def4ad294ffc21a4c5f6daed9e78952cbe
Zalexanninev15 <blue.shark@disroot.org>
2022-09-07 21:25:11 +0300

    Множество изменений (см. описание)
    - Добавлен новый вариант списка - NoADS_RU Extended, с поддержкой большего числа сайтов и элементов на них
    - Скрипт для генерации файла SITES.md теперь создаёт для каждого сайта ссылку (+ добавлена генерация ссылки на NoADS_RU Extended)
    - Мелкие улучшения в главном списке

--- assets/script.py
+++ assets/script.py
@@ -8,11 +8,12 @@ matches = re.finditer(regex, content, re.MULTILINE)
 links = []
 for matchNum, match in enumerate(matches, start=1):
     match = match.group()
-    lk = f"\n-{match}"
+    lk = f"\n- [{match.replace(' ', '')}]({match.replace(' ', '')})"
     links.append(lk)
 links_a = list(set(links))
-with open('SITES.md', 'w', encoding="utf8") as file:
-    file.write("\n# Сайты, которые имеются в списке NoADS_RU:\n\n")
-    for line in links_a:
+with open('SITES.md', 'w', encoding="utf8") as file: 
+    file.write("\n# Сайты, которые имеются в списке NoADS_RU:\n\n") 
+    for line in links_a: 
         file.write(line)
+    file.write('\n- И другие сайты (при использовании [расширенного варианта](https://raw.githubusercontent.com/Zalexanninev15/NoADS_RU/main/ads_list_extended.txt) списка)')
 print('Список пополнился новыми сайтами!')
